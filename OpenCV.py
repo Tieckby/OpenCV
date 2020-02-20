b17298f8e15f48588ed1258e1e3e8a287edcf9e7
Tiemoko <tiems@mail.com>
2020-02-20 14:50:29 +0000

    Modification

--- OpenCV.py
+++ OpenCV.py
@@ -21,22 +21,34 @@ import matplotlib.pyplot as plt
 # cv2.destroyAllWindows() #Ferme (détruit) la fenêtre apres le temps en milliseconde
 
 # #_____________________________FaceDetection__________________________
-face_cascade = cv2.CascadeClassifier("haar_cascade_files/haarcascade_frontalface_alt.xml")
-body_cascade = cv2.CascadeClassifier("haar_cascade_files/haarcascade_fullbody.xml")
-img = cv2.imread("yaya.jpg", 1)
+# face_cascade = cv2.CascadeClassifier("haar_cascade_files/haarcascade_frontalface_alt.xml")
+# img = cv2.imread("yaya.jpg", 1)
 
+# gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# faces = face_cascade.detectMultiScale(gray_img, scaleFactor = 1.5, minNeighbors=5)
+
+# for x, y, w, h in faces:
+#     img = cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 3)
+
+# resized = cv2.resize(img, (800, 700))
+
+# cv2.imshow("Tiémoko", resized)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+#_______________________________Human detection_____________________________
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+img = cv2.imread("yaya.jpg")
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-faces = face_cascade.detectMultiScale(gray_img, scaleFactor = 1.5, minNeighbors=5)
 
-body = body_cascade.detectMultiScale(img, scaleFactor=1.5, minNeighbors=4)
+boxes, weights = hog.detectMultiScale(img, winStride=(5, 5))
 
-for x, y, w, h in faces:
-    img = cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 3)
-
-for x, y, w, h in body:
+for x, y, w, h in boxes:
     cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 3)
 
-resized = cv2.resize(img, (800, 700))
+resized = cv2.resize(img, (600, 600))
 
 cv2.imshow("Tiémoko", resized)
 cv2.waitKey(0)
@@ -44,7 +56,6 @@ cv2.destroyAllWindows()
 
 # #_______________________VideoCamera Detecttion_________________________
 # face_cascade = cv2.CascadeClassifier("haar_cascade_files/haarcascade_frontalface_alt.xml")
-# # smile_cascade = cv2.CascadeClassifier("haar_cascade_files/haarcascade_smile1.xml")
 # body_cascade = cv2.CascadeClassifier("haar_cascade_files/haarcascade_fullbody.xml")
 # video = cv2.VideoCapture(0)
 
@@ -59,7 +70,6 @@ cv2.destroyAllWindows()
 #         ret, image = video.read()
 
 #         face = face_cascade.detectMultiScale(image, scaleFactor=1.5, minNeighbors=5)
-#         # smile = smile_cascade.detectMultiScale(image, scaleFactor=2.5, minNeighbors=20)
 #         body = body_cascade.detectMultiScale(image, scaleFactor=1.5, minNeighbors=4)
 
 #         # cv2.rectangle(image, start_point, end_point, color, thickness) : thickness->L'epaisseur de la ligne de bordure du rectangle en pixel
@@ -69,9 +79,6 @@ cv2.destroyAllWindows()
 #             font_name = cv2.FONT_HERSHEY_SIMPLEX
 #             color = (255, 255, 255)
 #             cv2.putText(image, name, (x, y-10), font_name, 1, color, 2, cv2.LINE_AA)
-
-#         # for x, y, w, h in smile:
-#         #     cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
 #         for x, y, w, h in body:
 #            cv2.rectangle(image, (x, y), (x+w, y+h), (255, 255, 255), 2)
